7c1a05181adafe151c9a8f10427b138d5ff2ef99
Abray0 <rayesabdou0@gmail.com>
2025-01-28 21:11:21 +0100

    Stats_moduleV2

---
+++ statistiques-service/src/test/java/com/example/statistiquesservice/StatistiquesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statistiquesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatistiquesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
